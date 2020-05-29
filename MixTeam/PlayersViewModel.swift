a4250c20d4e9843acab9fd59fc3045005f44c24d
Renaud Jenny <renaud.jenny@gmail.com>
2020-05-29 20:22:07 +0100

    refactor(Players): simplify the randomization teams code a lot!

--- MixTeam/PlayersViewModel.swift
+++ MixTeam/PlayersViewModel.swift
@@ -20,95 +20,23 @@ final class PlayersViewModel: ObservableObject {
     }
 }
 
-// TODO: Old code. Refactor and simplify this
 extension PlayersViewModel {
     private func randomizeTeam() {
-        var teamsHandicap: [Team: Int] = Dictionary(teams
-            .map({ team -> (Team, Int) in
-                let handicapSum = team.players
-                    .map(\.handicap)
-                    .reduce(0, +)
-                return (team, handicapSum)
-            }), uniquingKeysWith: { $1 })
+        let players = teams.flatMap(\.players)
+        guard players.count > 0 else { return }
+        let availableTeams = teams.filter { $0 != teams.first }
+        guard availableTeams.count > 1 else { return }
 
-        let playersTotalHandicap = teamsHandicap.values.reduce(0, +)
+        teams.forEach { $0.players = [] }
 
-        // Move all players in the standing team
-        guard let standingPlayersTeam = teams.first else { return }
-        teams
-            .filter({ $0 != standingPlayersTeam })
-            .forEach({ team in
-                team.players.forEach { player in
-                    move(player: player, from: team, to: standingPlayersTeam)
-                }
-            })
-
-        for team in teams {
-            for player in team.players {
-                guard let toTeam = pseudoRandomTeam(teamsHandicap: teamsHandicap, playersTotalHandicap: playersTotalHandicap) else {
-                    continue
-                }
-                teamsHandicap[team]! -= player.handicap
-                teamsHandicap[toTeam]! += player.handicap
-                if team != toTeam {
-                    self.move(player: player, from: team, to: toTeam)
-                }
-            }
+        players.shuffled().forEach {
+            availableTeams.sorted(by: hasLessPlayer).first?.players.append($0)
         }
+        teams.first?.players = []
         objectWillChange.send()
     }
 
-    private func move(player: Player, from fromTeam: Team, to toTeam: Team) {
-        guard let originPlayerIndex = fromTeam.players.firstIndex(where: { $0.id == player.id }) else {
-            return
-        }
-
-        toTeam.players.append(player)
-        fromTeam.players.remove(at: originPlayerIndex)
-
-        // deferAutoSave()
-    }
-
-    private func pseudoRandomTeam(teamsHandicap: [Team: Int], playersTotalHandicap: Int) -> Team? {
-        // First, add a player in each team if there is no one yet
-        let teamsWithoutPlayers = teams
-            .filter { $0 != teams.first }
-            .filter { $0.players.count == 0 }
-
-        if teamsWithoutPlayers.count > 0 {
-            let randomIndex = Int.random(in: 0..<teamsWithoutPlayers.count)
-            return teamsWithoutPlayers[randomIndex]
-        }
-
-        let handicapAverage = playersTotalHandicap / (teams.count - 1)
-
-        // Choose only teams that total handicap is under the average
-        let unbalancedTeams = teams
-            .filter { $0 != teams.first }
-            .filter { teamsHandicap[$0]! < handicapAverage }
-
-        guard unbalancedTeams.count > 0 else { return nil }
-
-        let randomIndex = Int.random(in: 0..<unbalancedTeams.count)
-        return unbalancedTeams[randomIndex]
-    }
-
-    private func remove(team: Team) {
-        guard let firstTeam = teams.first,
-            let teamToDelete = teams.first(where: { $0 == team }) else {
-                fatalError("Cannot retrieve first team or team to delete index")
-        }
-
-        teamToDelete.players.forEach { (player) in
-            self.move(player: player, from: teamToDelete, to: firstTeam)
-        }
-
-        teams.removeAll(where: { $0 == team })
-    }
-
-    private func debug() {
-        print(teams
-            .map { ($0.name, $0.players.map(\.name)) }
-        )
+    private func hasLessPlayer(teamA a: Team, teamB b: Team) -> Bool {
+        return a.players.count < b.players.count
     }
 }
