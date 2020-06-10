e0a6e7e75a016d6d451603085c881fd1eb88a153
Renaud Jenny <renaud.jenny@gmail.com>
2020-06-10 18:38:18 +0100

    feat(Teams): improve Team row style

--- MixTeam/TeamsView.swift
+++ MixTeam/TeamsView.swift
@@ -4,13 +4,36 @@ struct TeamsView: View {
     @ObservedObject var viewModel = TeamsViewModel()
 
     var body: some View {
-        List {
-            ForEach(viewModel.teams, content: teamRow)
+        NavigationView {
+            teamsView
         }
     }
 
+    private var teamsView: some View {
+        List {
+            ForEach(viewModel.teams, content: teamRow)
+        }
+        .listStyle(GroupedListStyle())
+        .navigationBarTitle("Players")
+    }
+
     private func teamRow(team: Team) -> some View {
-        Text(team.name)
+        Button(action: { }) {
+            HStack {
+                team.image?.imageIdentifier.image
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .padding(.leading, 20)
+                    .padding(.trailing)
+                Text(team.name)
+                Spacer()
+            }.foregroundColor(Color(team.color.color))
+        }
+        .buttonStyle(DefaultButtonStyle())
+        .padding([.top, .bottom], 10)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .listRowInsets(EdgeInsets())
+        .background(Color(team.color.color).opacity(0.10))
     }
 }
 
