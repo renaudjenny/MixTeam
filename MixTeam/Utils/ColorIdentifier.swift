8b8f996c3d57794c29154aaecb0249858e9d48a2
Renaud Jenny <renaud.jenny@gmail.com>
2020-06-16 18:48:41 +0100

    feat(Utils): replace RGB color by default SwiftUI ones

--- MixTeam/Utils/ColorIdentifier.swift
+++ MixTeam/Utils/ColorIdentifier.swift
@@ -4,24 +4,22 @@ enum ColorIdentifier: String, Identifiable, CaseIterable, Codable {
     case yellow
     case orange
     case red
-    case maroon
+    case pink
     case purple
-    case azure
-    case jade
-    case lime
+    case blue
+    case green
     case gray
 
     var color: Color {
         switch self {
-        case .yellow: return Color(red:0.95, green:0.98, blue:0.10)
-        case .orange: return Color(red:0.90, green:0.50, blue:0.10)
-        case .red: return Color(red:0.90, green:0.00, blue:0.24)
-        case .maroon: return Color(red:0.59, green:0.00, blue:0.21)
-        case .purple: return Color(red:0.37, green:0.00, blue:0.55)
-        case .azure: return Color(red:0.27, green:0.50, blue:1.00)
-        case .jade: return Color(red:0.27, green:0.64, blue:0.59)
-        case .lime: return Color(red:0.38, green:0.89, blue:0.29)
-        case .gray: return Color(red:0.5, green:0.5, blue:0.5)
+        case .yellow: return .yellow
+        case .orange: return .orange
+        case .red: return .red
+        case .pink: return .pink
+        case .purple: return .purple
+        case .blue: return .blue
+        case .green: return .green
+        case .gray: return .gray
         }
     }
 
@@ -30,11 +28,10 @@ enum ColorIdentifier: String, Identifiable, CaseIterable, Codable {
         case .yellow: return "yellow"
         case .orange: return "orange"
         case .red: return "red"
-        case .maroon: return "maroon"
+        case .pink: return "pink"
         case .purple: return "purple"
-        case .azure: return "azure"
-        case .jade: return "jade"
-        case .lime: return "lime"
+        case .blue: return "blue"
+        case .green: return "green"
         case .gray: return "gray"
         }
     }
