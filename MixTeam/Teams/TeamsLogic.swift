384d5d73c086b94f391b7af628ffbc54f5c2568c
Renaud Jenny <renaud.jenny@gmail.com>
2020-07-13 18:53:03 +0100

    refactor(Teams): remove useless methods from TeamLogic

--- MixTeam/Teams/TeamsLogic.swift
+++ MixTeam/Teams/TeamsLogic.swift
@@ -5,34 +5,25 @@ protocol TeamsLogic {
     var teamsStore: TeamsStore { get }
     var teamsList: ArraySlice<Team> { get }
 
-    // TODO: at the end remove createTeam(_:)
-    func createTeam(_ team: Team)
     func createRandomTeam()
     func editTeam(_ team: Team)
     func deleteTeam(_ team: Team)
-    // TODO: at the end remove deleteTeam(atOffsets offsets:)
-    func deleteTeam(atOffsets offsets: IndexSet)
     func isFirstTeam(_ team: Team) -> Bool
 }
 
 extension TeamsLogic {
     var teamsList: ArraySlice<Team> { teamsStore.teams.dropFirst() }
 
-    func createTeam(_ team: Team) {
-        teamsStore.teams.append(team)
-    }
-
     func createRandomTeam() {
-        teamsStore.teams.append(randomTeam)
-    }
+        let image = ImageIdentifier.teams.randomElement() ?? .koala
+        let color = ColorIdentifier.allCases.randomElement() ?? .red
+        let name = "\(color.name) \(image.name)".localizedCapitalized
 
-    func deleteTeam(atOffsets offsets: IndexSet) {
-        // As the real first team is not displayed. The real index is offset + 1
-        let index = (offsets.first ?? 0) + 1
-        let playersInDeletedTeam = teamsStore.teams[index].players
-        teamsStore.teams[0].players.append(contentsOf: playersInDeletedTeam)
-
-        teamsStore.teams.remove(at: index)
+        teamsStore.teams.append(Team(
+            name: name,
+            colorIdentifier: color,
+            imageIdentifier: image
+        ))
     }
 
     func deleteTeam(_ team: Team) {
@@ -53,12 +44,4 @@ extension TeamsLogic {
     }
 
     func isFirstTeam(_ team: Team) -> Bool { teamsStore.teams.first == team }
-
-    private var randomTeam: Team {
-        let image = ImageIdentifier.teams.randomElement() ?? .koala
-        let color = ColorIdentifier.allCases.randomElement() ?? .red
-        let name = "\(color.name) \(image.name)".localizedCapitalized
-
-        return Team(name: name, colorIdentifier: color, imageIdentifier: image)
-    }
 }
