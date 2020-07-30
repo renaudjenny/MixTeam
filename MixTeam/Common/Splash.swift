34081eac6e2146107539f50e2aca3efb35b76d79
Renaud Jenny <renaud.jenny@gmail.com>
2020-07-30 22:19:51 +0100

    refactor(Splash): redraw a new Splash Shape from scratch

--- MixTeam/Common/Splash.swift
+++ MixTeam/Common/Splash.swift
@@ -5,42 +5,219 @@ struct Splash: Shape {
         var path = Path()
 
         let point1 = CGPoint(
-            x: rect.minX + rect.maxX * 6/100,
-            y: rect.minY + rect.maxY * 41/100
+            x: rect.minX + rect.maxX * 140/1000,
+            y: rect.minY + rect.maxY * 325/1000
         )
         path.move(to: point1)
 
         let point2 = CGPoint(
-            x: rect.minX + rect.maxX * 28/100,
-            y: rect.minY + rect.maxY * 41/100
+            x: rect.minX + rect.maxX * 200/1000,
+            y: rect.minY + rect.maxY * 485/1000
         )
-        path.addCurve(
+        path.addQuadCurve(
             to: point2,
-            control1: CGPoint(
-                x: rect.minX + rect.maxX * 8/100,
-                y: rect.minY + rect.maxY * 36/100
-            ),
-            control2: CGPoint(
-                x: rect.minX + rect.maxX * 28/100,
-                y: rect.minY + rect.maxY * 47/100
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 275/1000,
+                y: rect.minY + rect.maxY * 435/1000
             )
         )
 
         let point3 = CGPoint(
-            x: rect.minX + rect.maxX * 21/100,
-            y: rect.minY + rect.maxY * 31/100
+            x: rect.minX + rect.maxX * 270/1000,
+            y: rect.minY + rect.maxY * 560/1000
         )
-        path.addCurve(
+        path.addQuadCurve(
             to: point3,
-            control1: CGPoint(
-                x: rect.minX + rect.maxX * 26/100,
-                y: rect.minY + rect.maxY * 36/100
-            ),
-            control2: CGPoint(
-                x: rect.minX + rect.maxX * 19/100,
-                y: rect.minY + rect.maxY * 35/100
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 120/1000,
+                y: rect.minY + rect.maxY * 550/1000
             )
         )
+
+        let point4 = CGPoint(
+            x: rect.minX + rect.maxX * 300/1000,
+            y: rect.minY + rect.maxY * 620/1000
+        )
+        path.addQuadCurve(
+            to: point4,
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 330/1000,
+                y: rect.minY + rect.maxY * 570/1000
+            )
+        )
+
+        let point5 = CGPoint(
+            x: rect.minX + rect.maxX * 330/1000,
+            y: rect.minY + rect.maxY * 660/1000
+        )
+        path.addQuadCurve(
+            to: point5,
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 275/1000,
+                y: rect.minY + rect.maxY * 685/1000
+            )
+        )
+
+        let point6 = CGPoint(
+            x: rect.minX + rect.maxX * 450/1000,
+            y: rect.minY + rect.maxY * 730/1000
+        )
+        path.addCurve(
+            to: point6,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 400/1000,
+                y: rect.minY + rect.maxY * 600/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 475/1000,
+                y: rect.minY + rect.maxY * 645/1000
+            )
+        )
+
+        let point7 = CGPoint(
+            x: rect.minX + rect.maxX * 435/1000,
+            y: rect.minY + rect.maxY * 950/1000
+        )
+        path.addQuadCurve(
+            to: point7,
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 420/1000,
+                y: rect.minY + rect.maxY * 860/1000
+            )
+        )
+
+        let point8 = CGPoint(
+            x: rect.minX + rect.maxX * 510/1000,
+            y: rect.minY + rect.maxY * 930/1000
+        )
+        path.addCurve(
+            to: point8,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 440/1000,
+                y: rect.minY + rect.maxY * 1010/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 500/1000,
+                y: rect.minY + rect.maxY * 1000/1000
+            )
+        )
+
+        let point9 = CGPoint(
+            x: rect.minX + rect.maxX * 495/1000,
+            y: rect.minY + rect.maxY * 690/1000
+        )
+        path.addQuadCurve(
+            to: point9,
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 515/1000,
+                y: rect.minY + rect.maxY * 890/1000
+            )
+        )
+
+        let point10 = CGPoint(
+            x: rect.minX + rect.maxX * 570/1000,
+            y: rect.minY + rect.maxY * 680/1000
+        )
+        path.addCurve(
+            to: point10,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 490/1000,
+                y: rect.minY + rect.maxY * 630/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 570/1000,
+                y: rect.minY + rect.maxY * 630/1000
+            )
+        )
+
+        let point11 = CGPoint(
+            x: rect.minX + rect.maxX * 560/1000,
+            y: rect.minY + rect.maxY * 790/1000
+        )
+        path.addQuadCurve(
+            to: point11,
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 555/1000,
+                y: rect.minY + rect.maxY * 770/1000
+            )
+        )
+
+        let point12 = CGPoint(
+            x: rect.minX + rect.maxX * 625/1000,
+            y: rect.minY + rect.maxY * 780/1000
+        )
+        path.addCurve(
+            to: point12,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 570/1000,
+                y: rect.minY + rect.maxY * 840/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 625/1000,
+                y: rect.minY + rect.maxY * 840/1000
+            )
+        )
+
+        let point13 = CGPoint(
+            x: rect.minX + rect.maxX * 640/1000,
+            y: rect.minY + rect.maxY * 580/1000
+        )
+        path.addCurve(
+            to: point13,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 620/1000,
+                y: rect.minY + rect.maxY * 650/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 560/1000,
+                y: rect.minY + rect.maxY * 610/1000
+            )
+        )
+
+        let point14 = CGPoint(
+            x: rect.minX + rect.maxX * 725/1000,
+            y: rect.minY + rect.maxY * 600/1000
+        )
+        path.addQuadCurve(
+            to: point14,
+            control: CGPoint(
+                x: rect.minX + rect.maxX * 680/1000,
+                y: rect.minY + rect.maxY * 560/1000
+            )
+        )
+
+        let point15 = CGPoint(
+            x: rect.minX + rect.maxX * 740/1000,
+            y: rect.minY + rect.maxY * 495/1000
+        )
+        path.addCurve(
+            to: point15,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 695/1000,
+                y: rect.minY + rect.maxY * 520/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 705/1000,
+                y: rect.minY + rect.maxY * 490/1000
+            )
+        )
+
+        let point16 = CGPoint(
+            x: rect.minX + rect.maxX * 780/1000,
+            y: rect.minY + rect.maxY * 420/1000
+        )
+        path.addCurve(
+            to: point16,
+            control1: CGPoint(
+                x: rect.minX + rect.maxX * 810/1000,
+                y: rect.minY + rect.maxY * 505/1000
+            ),
+            control2: CGPoint(
+                x: rect.minX + rect.maxX * 820/1000,
+                y: rect.minY + rect.maxY * 455/1000
+            )
+        )
+
         return path
     }
 }
@@ -64,7 +241,7 @@ struct Splash_Previews: PreviewProvider {
                             .scale(1/10)
                         Image("splash")
                         .resizable()
-                        .opacity(1/2)
+                        .opacity(1/4)
                     }
                     .frame(width: self.width, height: self.height)
                     .border(Color.red)
