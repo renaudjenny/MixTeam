97abe4ba1256d2167d26368c3ecc3ddc3c43c929
Renaud Jenny <renaud.jenny@gmail.com>
2021-06-16 22:18:47 +0100

    feat(Scores): improve ScoreboardView
    * Clean the code to be more readable
    * Improve the way we handle Round and Teams

--- MixTeam/Scores/ScoreboardView.swift
+++ MixTeam/Scores/ScoreboardView.swift
@@ -1,39 +1,64 @@
 import SwiftUI
 
 struct ScoreboardView: View {
-    let scoreLines: [ScoreLine]
-    let teams: [Team]
+    let rounds: [Round]
 
     var body: some View {
-        VStack {
-            HStack {
-                EmptyView().frame(width: 100)
-                ForEach(teams) {
-                    Text("\($0.name)").frame(width: 100)
-                }
+        List {
+            Section(header: HeaderView(rounds: rounds)) {
+                ForEach(rounds, content: RoundView.init)
             }
-            List(scoreLines, rowContent: ScoreLineView.init)
         }
     }
 }
 
-struct ScoreLineView: View {
-    let scoreLine: ScoreLine
+struct HeaderView: View {
+    let rounds: [Round]
 
     var body: some View {
         HStack {
-            Text(scoreLine.name).frame(width: 100)
-            ForEach(scoreLine.scores) {
-                Text("\($0.points)").frame(width: 100)
+            Color.clear.frame(width: 100)
+            ForEach(teams) {
+                Text("\($0.name)")
+                    .frame(width: 100)
+                    .multilineTextAlignment(.center)
+            }
+        }
+    }
+
+    var teams: [Team] {
+        rounds
+            .flatMap(\.scores)
+            .map(\.team)
+            .reduce([], {
+                guard !$0.contains($1)
+                else { return $0 }
+                return $0 + [$1]
+            })
+    }
+}
+
+struct RoundView: View {
+    let round: Round
+
+    var body: some View {
+        HStack {
+            Text(round.name)
+                .frame(width: 100)
+                .multilineTextAlignment(.trailing)
+            ForEach(round.scores) {
+                Text("\($0.points)")
+                    .frame(width: 100)
+                    .multilineTextAlignment(.center)
             }
         }
     }
 }
 
-struct ScoreLine: Identifiable {
+struct Round: Identifiable {
     let name: String
     let scores: [Score]
-    var id: String { name }
+    let id = UUID()
 
     struct Score: Identifiable {
         var team: Team
@@ -44,38 +69,35 @@ struct ScoreLine: Identifiable {
 
 struct ScoreboardView_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreboardView(
-            scoreLines: .mock,
-            teams: Array([Team].exampleTeam.dropFirst())
-        )
+        ScoreboardView(rounds: .mock)
     }
 }
 
-extension Array where Element == ScoreLine {
+extension Array where Element == Round {
     static let team1: Team = [Team].exampleTeam[1]
     static let team2: Team = [Team].exampleTeam[2]
 
     static let mock: Self = {
         [
-            ScoreLine(
-                name: "Game 1",
+            Round(
+                name: "Round 1",
                 scores: [
-                    ScoreLine.Score(team: team1, points: 0),
-                    ScoreLine.Score(team: team2, points: 20),
+                    Round.Score(team: team1, points: 0),
+                    Round.Score(team: team2, points: 20),
                 ]
             ),
-            ScoreLine(
-                name: "Game 2",
+            Round(
+                name: "Round 2",
                 scores: [
-                    ScoreLine.Score(team: team1, points: 10),
-                    ScoreLine.Score(team: team2, points: 20),
+                    Round.Score(team: team1, points: 10),
+                    Round.Score(team: team2, points: 20),
                 ]
             ),
-            ScoreLine(
-                name: "Game 3",
+            Round(
+                name: "Round 3",
                 scores: [
-                    ScoreLine.Score(team: team1, points: 10),
-                    ScoreLine.Score(team: team2, points: 50),
+                    Round.Score(team: team1, points: 10),
+                    Round.Score(team: team2, points: 50),
                 ]
             ),
         ]
