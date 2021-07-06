b4daa2282c4d4fd8b72f89ca305789829565cd28
Renaud Jenny <renaud.jenny@gmail.com>
2021-07-06 19:19:20 +0100

    feat(Scores): add new design for Total Section

--- MixTeam/Scores/TotalScoresView.swift
+++ MixTeam/Scores/TotalScoresView.swift
@@ -4,18 +4,38 @@ struct TotalScoresView: View {
     let rounds: [Round]
 
     var body: some View {
-        Section(header: Text("Total")) {
+        Section(header: header) {
             ForEach(rounds.teams) { team in
                 HStack {
                     Text("\(team.name)")
-                        .bold()
-                        .frame(width: 100)
-                    Text(total(for: team))
-                        .bold()
-                        .frame(width: 100)
+                    Spacer()
+                    ZStack {
+                        Text("99999").hidden()
+                        Text(total(for: team))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal, 4)
+                    .padding(.vertical, 4)
+                    .background(Color.purple.clipShape(Ellipse()))
                 }
             }
+            .listRowBackground(Color.purple.opacity(20/100))
         }
+    }
+
+    private var header: some View {
+        HStack {
+        Text("Total")
+            .font(.title3)
+            .fontWeight(.heavy)
+            .foregroundColor(.white)
+            Spacer()
+        }
+        .listRowInsets(EdgeInsets())
+        .padding(.horizontal)
+        .padding(.vertical, 8)
+        .background(Color.purple)
     }
 
     private func total(for team: Team) -> String {
@@ -32,7 +52,9 @@ struct TotalScoresView: View {
 #if DEBUG
 struct TotalScoresView_Previews: PreviewProvider {
     static var previews: some View {
-        TotalScoresView(rounds: .mock)
+        List {
+            TotalScoresView(rounds: .mock)
+        }
     }
 }
 #endif
