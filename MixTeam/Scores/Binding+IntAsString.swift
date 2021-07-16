345eccd656d3b88df28aaf9f99a932fe0e483541
Renaud Jenny <renaud.jenny@gmail.com>
2021-07-16 07:58:59 +0100

    fix(Scores): removing all characters in score field works properly

--- MixTeam/Scores/Binding+IntAsString.swift
+++ MixTeam/Scores/Binding+IntAsString.swift
@@ -4,7 +4,10 @@ extension Binding where Value == Int {
     var string: Binding<String> {
         Binding<String>(
             get: { String(wrappedValue) },
-            set: { wrappedValue = Int($0) ?? wrappedValue }
+            set: {
+                guard $0 != "" else { return }
+                wrappedValue = Int($0) ?? wrappedValue
+            }
         )
     }
 }
