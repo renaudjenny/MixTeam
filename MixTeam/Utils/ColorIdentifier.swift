88cea2d9a49183124ac048285650ee2931fba0c2
Renaud Jenny <renaud.jenny@gmail.com>
2022-11-11 11:22:06 +0000

    feat(Color): add preview for colors

--- MixTeam/Utils/ColorIdentifier.swift
+++ MixTeam/Utils/ColorIdentifier.swift
@@ -38,3 +38,39 @@ enum ColorIdentifier: String, Identifiable, CaseIterable, Codable {
 
     var id: Int { hashValue }
 }
+
+#if DEBUG
+struct Colors_Previews: PreviewProvider {
+    static var previews: some View {
+        let colors: [[Color]] = ColorIdentifier.allCases.enumerated().reduce(into: [[], []]) { result, next in
+            if next.offset.isMultiple(of: 2) {
+                result[0].append(next.element.color)
+            } else {
+                result[1].append(next.element.color)
+            }
+        }
+        ScrollView {
+            HStack {
+                ForEach(colors, id: \.hashValue) { colorColumn in
+                    VStack {
+                        ForEach(colorColumn, id: \.hashValue) { color in
+                            preview(for: color)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static func preview(for color: Color) -> some View {
+        VStack {
+            Text("Lorem Ipsum")
+                .font(.title)
+            Text("Smaller text")
+            
+        }
+        .frame(width: 180, height: 200)
+        .background(color)
+    }
+}
+#endif
