0ce1133379af784ea5e3536de5ad485cfe1455d9
Renaud Jenny <renaud.jenny@gmail.com>
2023-02-14 20:27:57 +0000

    feat(App): add unit test for `App` logic

---
+++ MixTeamTests/App/AppTests.swift
@@ -0,0 +1,36 @@
+import ComposableArchitecture
+import XCTest
+@testable import MixTeam
+
+class AppTests: XCTestCase {
+    @MainActor
+    func testSelectTab() async throws {
+        let store = TestStore(initialState: .example, reducer: App())
+
+        await store.send(.tabSelected(.scoreboard)) {
+            $0.selectedTab = .scoreboard
+        }
+
+        await store.send(.tabSelected(.settings)) {
+            $0.selectedTab = .settings
+        }
+
+        await store.send(.tabSelected(.compositionLoader)) {
+            $0.selectedTab = .compositionLoader
+        }
+    }
+
+    @MainActor
+    func testTask() async throws {
+        let store = TestStore(initialState: .example, reducer: App())
+
+        let migrationV2toV3Expectation = self.expectation(description: "Migration V2 to V3 being called")
+        store.dependencies.migration.v2toV3 = { migrationV2toV3Expectation.fulfill() }
+
+        let migrationV3_0toV3_1Expectation = self.expectation(description: "Migration V3.0 to V3.1 being called")
+        store.dependencies.migration.v3_0toV3_1 = { migrationV3_0toV3_1Expectation.fulfill() }
+
+        await store.send(.task)
+        wait(for: [migrationV2toV3Expectation, migrationV3_0toV3_1Expectation], timeout: 0.1)
+    }
+}
