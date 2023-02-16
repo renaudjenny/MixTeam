18901010c8350f25e11da5e055314552c1c9f23e
Renaud Jenny <renaud.jenny@gmail.com>
2023-02-16 21:13:48 +0000

    feat(Composition): add test for Archiving Team action

--- MixTeamTests/Composition/CompositionTests.swift
+++ MixTeamTests/Composition/CompositionTests.swift
@@ -33,11 +33,15 @@ class CompositionTests: XCTestCase {
         await store.finish(timeout: 1)
     }
 
-    func testMixTeamAndConfirmationIsPresented() throws {
+    func testMixTeamAndConfirmationIsPresented() async throws {
         let store = TestStore(initialState: Composition.State(), reducer: Composition())
 
-        store.send(.mixTeam) {
+        await store.send(.mixTeam) {
             $0.notEnoughTeamsConfirmationDialog = .notEnoughTeams
+        }
+
+        await store.send(.dismissNotEnoughTeamsAlert) {
+            $0.notEnoughTeamsConfirmationDialog = nil
         }
     }
 
@@ -55,5 +59,25 @@ class CompositionTests: XCTestCase {
             $0.teams = [Team.State(id: id, name: "Strawberry Bunny", color: .strawberry, image: .bunny)]
         }
         wait(for: [addTeamToPersistenceExpectation], timeout: 0.1)
+    }
+
+    func testArchiveTeam() async throws {
+        let store = TestStore(initialState: .example, reducer: Composition())
+
+        let teams = store.state.teams
+        let indexSet = IndexSet(integer: 0)
+
+        let updateTeamsExpectation = expectation(description: "Update teams")
+        store.dependencies.teamPersistence.updateValues = { archivedTeams in
+            XCTAssertEqual(archivedTeams.elements.map(\.id), indexSet.map { teams[$0] }.map(\.id))
+            XCTAssert(archivedTeams.allSatisfy(\.isArchived), "All archived teams should be in archived status")
+            updateTeamsExpectation.fulfill()
+        }
+
+        await store.send(.archiveTeams(indexSet)) {
+            $0.teams.remove(atOffsets: indexSet)
+        }
+
+        wait(for: [updateTeamsExpectation], timeout: 0.1)
     }
 }
