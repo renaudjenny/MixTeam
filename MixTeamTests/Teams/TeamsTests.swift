e7f885382f50423456c81b2d76b8b3c100b57121
Renaud Jenny <renaud.jenny@gmail.com>
2023-02-19 11:40:34 +0000

    feat(Team): add logic tests

--- MixTeamTests/Teams/TeamsTests.swift
+++ MixTeamTests/Teams/TeamsTests.swift
@@ -21,4 +21,42 @@ final class TeamsTests: XCTestCase {
         }
         wait(for: [updateExpectation], timeout: 0.1)
     }
+
+    func testUpdateName() async throws {
+        let store = TestStore(initialState: Team.State.previewWithPlayers, reducer: Team())
+
+        let updateExpectation = expectation(description: "Update team persistence")
+        store.dependencies.teamPersistence.updateOrAppend = { _ in updateExpectation.fulfill() }
+
+        await store.send(.binding(.set(\.$name, "Test"))) {
+            $0.name = "Test"
+        }
+        wait(for: [updateExpectation], timeout: 0.1)
+    }
+
+    func testUpdateImage() async throws {
+        let store = TestStore(initialState: Team.State.previewWithPlayers, reducer: Team())
+
+        let updateExpectation = expectation(description: "Update team persistence")
+        store.dependencies.teamPersistence.updateOrAppend = { _ in updateExpectation.fulfill() }
+
+        await store.send(.binding(.set(\.$image, .clown))) {
+            $0.image = .clown
+        }
+        wait(for: [updateExpectation], timeout: 0.1)
+    }
+
+    func testMoveBackPlayer() async throws {
+        let store = TestStore(initialState: Team.State.previewWithPlayers, reducer: Team())
+
+        let updateExpectation = expectation(description: "Update team persistence")
+        store.dependencies.teamPersistence.updateOrAppend = { _ in updateExpectation.fulfill() }
+
+        let firstPlayerID = store.state.players.first?.id ?? UUID()
+
+        await store.send(.moveBackPlayer(id: firstPlayerID)) {
+            $0.players.remove(id: firstPlayerID)
+        }
+        wait(for: [updateExpectation], timeout: 0.1)
+    }
 }
