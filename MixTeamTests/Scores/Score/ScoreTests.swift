f9ff0bc4cdc3da55d15e406a22196ab7e671552d
Renaud Jenny <renaud.jenny@gmail.com>
2023-02-23 18:51:40 +0000

    feat(Score): add test for Score logic

---
+++ MixTeamTests/Scores/Score/ScoreTests.swift
@@ -0,0 +1,20 @@
+import ComposableArchitecture
+@testable import MixTeam
+import XCTest
+
+@MainActor
+final class ScoreTests: XCTestCase {
+    func testSetPoints() async {
+        let updateScoreExpectation = expectation(description: "Update score")
+        let uuid: UUIDGenerator = .incrementing
+        let state = Score.State(id: uuid(), team: .preview)
+        let store = TestStore(initialState: state, reducer: Score()) { dependencies in
+            dependencies.scoresPersistence.updateScore = { _ in updateScoreExpectation.fulfill() }
+        }
+
+        await store.send(.binding(.set(\.$points, 123))) {
+            $0.points = 123
+        }
+        wait(for: [updateScoreExpectation], timeout: 0.1)
+    }
+}
